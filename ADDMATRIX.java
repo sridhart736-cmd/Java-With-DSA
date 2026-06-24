1172cbe6f01452289a92850a83561852fa3d2e12
sridhart736-cmd <sridhart736@gmail.com>
2026-06-24 15:45:57 +0530

    Update and rename ADD.java to ADDMATRIX.java

--- ADDMATRIX.java
+++ ADDMATRIX.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ADD {
+public class ADDMATRIX {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] a = new int[3][3];
