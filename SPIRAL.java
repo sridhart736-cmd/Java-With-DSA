bc8c41c7b570f03b0c2e3b5f98811b3f78e9cea2
sridhart736-cmd <sridhart736@gmail.com>
2026-06-24 15:52:06 +0530

    Add files via upload

---
+++ SPIRAL.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SPIRAL{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int top = 0, bottom = 2, left = 0, right = 2;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+}
