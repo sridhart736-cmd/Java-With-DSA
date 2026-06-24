c7d80c58ff52a201e30e4880ee66006d990ca317
sridhart736-cmd <sridhart736@gmail.com>
2026-06-24 15:45:13 +0530

    Add files via upload

---
+++ ADD.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ADD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] a = new int[3][3];
+        int[][] b = new int[3][3];
+        int[][] sum = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
